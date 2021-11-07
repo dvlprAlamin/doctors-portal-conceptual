5c148d071e95ca8481db94e1d6226fa9aa4d3d57
dvlprAlamin <alaminhowladerofficial@gmail.com>
2021-11-08 00:14:16 +0600

    updated

--- src/firebase/firebase.config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDHC93L6BB-i0kdInm79wQy0aXl0T-7rWg",
-    authDomain: "real-doctors-portal.firebaseapp.com",
-    projectId: "real-doctors-portal",
-    storageBucket: "real-doctors-portal.appspot.com",
-    messagingSenderId: "996718008758",
-    appId: "1:996718008758:web:bc3bbb2ed7cac39baf0917"
-};
-export default firebaseConfig;
